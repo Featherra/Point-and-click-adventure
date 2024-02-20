9a71883f830d0a420abc2b7194846bf46c8c1d4c
Featherra <v.elfferich@sintlucasedu.nl>
2024-02-20 12:32:27 +0100

    Fixes, new stuff
    fixed issues and added tile map, and interacting with objects.
    
    for example, when there's a key in your inventory you're able to open the door (the building on the left). Else you're not able to enter.

--- js/main.js
+++ js/main.js
@@ -4,13 +4,15 @@ document.getElementById("mainTitle").innerText = "Point and Click Adventure Game
 const gameWindow = document.getElementById("gameWindow");
 
 //inventory
+let inventory = [];
+console.log(inventory);
 const inventoryList = document.getElementById("inventoryList");
 
 //Main Character
 const mainCharacter = document.getElementById("hero");
 const offsetCharacter = 16;
 
-
+//Decor/interactables
 const tree1 = document.getElementById("squareTree");
 const door1 = document.getElementById("door");
 
@@ -19,29 +21,36 @@ gameWindow.onclick = function (e) {
     var x = e.clientX - rect.left;
     var y = e.clientY - rect.top;
     //TODO: calc offsat  based on character size
-    //FIX: character doesn't animate right on the first click.
-    //FIX: character can go out of bounnds when clicking on itself (i don't have that issue)
-    mainCharacter.style.left = x - offsetCharacter + "px";
-    mainCharacter.style.top = y - offsetCharacter + "px";
+
+    if (e.target.id !== "heroimg") {
+        mainCharacter.style.left = x - offsetCharacter + "px";
+        mainCharacter.style.top = y - offsetCharacter + "px";
+    }
 
     switch (e.target.id) { // basically same as if statements but easier/less messy
-        case "squareTree":
-            tree1.style.opacity = 0.5;
+        //case "squareTree":
+        //  tree1.style.opacity = 0.5;
 
-        //case "door":
-        // console.log("Clicked door");
-        // break;
+
         case "key":
-            console.log('You\'ve found a key!');
-
-            document.getElementById("key").remove();
-            const keyElement = document.createElement("li");
-            keyElement.id = "inv-key";
-            keyElement.innerText = "key";
-            inventoryList.appendChild(keyElement);
+            getItem("Rusty key", "rustyKey");
+            break;
+        case "well":
+            getItem("Coin", "coin");
+            break;
+        case "doorWizardHut": // at the door wizard hut
+            if (checkItem("Rusty key")) { // check if you have the rusty key
+                console.log("I opened the door. Yeah!");
+            } else if (checkItem("Coin")) {
+                removeItem("Coin", "coin");
+                console.log("I tried to open the door with the coin, it didn't work. And it slipped through the keyhole.");
+            } else { //else 
+                console.log("Fuck, this door's locked. I need to find a key.");
+            }
             break;
         default:
-            tree1.style.opacity = 1;
+            break;
+        // tree1.style.opacity = 1;
     }
     // ^ does basically what's below 
     // if (e.target.id == "squareTree") {
@@ -50,5 +59,47 @@ gameWindow.onclick = function (e) {
     //     tree1.style.opacity = 1;
     // } 
 
+    /**
+     * Checks if the  value exists within the array
+     * If not then it adds value to the array and use showItem function
+     * @param {string} itemName 
+     * @param {string} itemId 
+     */
+    function getItem(itemName, itemId) {
+        if (!checkItem(itemName)) {
+            inventory.push(itemName);
+            showItem(itemName, itemId);
+        }
+        console.log(inventory);
 
+    }
+
+    function checkItem(itemName) {
+        return inventory.includes(itemName);
+    }
+    /**
+     * Needs a name for displaying item and a htmml id name
+     * @param {string} itemName 
+     * @param {string} itemId 
+     */
+    function showItem(itemName, itemId) {
+        console.log('You\'ve found a ' + itemName + '!');
+        const keyElement = document.createElement("li");
+        keyElement.id = itemId;
+        keyElement.innerText = itemName;
+        inventoryList.appendChild(keyElement);
+    }
+
+    /**
+     * removes 
+     * @param {*} itemName 
+     * @param {*} itemId 
+     */
+    function removeItem(itemName, itemId) {
+        // remove item in array
+        inventory == inventory.filter(function (newInventory) {
+            return newInventory !== itemName;
+        });
+        document.getElementById(itemId).remove();
+    }
 }
